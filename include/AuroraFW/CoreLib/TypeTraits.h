d3b8766f4bdd66762c79230a7bc0669efb93924b
Luís Ferreira <lsferreira169@gmail.com>
2018-02-15 00:06:41 +0000

    Implement a hacky converter for C++ to C function callbacks

---
+++ include/AuroraFW/CoreLib/TypeTraits.h
@@ -0,0 +1,41 @@
+/****************************************************************************
+** ┌─┐┬ ┬┬─┐┌─┐┬─┐┌─┐  ┌─┐┬─┐┌─┐┌┬┐┌─┐┬ ┬┌─┐┬─┐┬┌─
+** ├─┤│ │├┬┘│ │├┬┘├─┤  ├┤ ├┬┘├─┤│││├┤ ││││ │├┬┘├┴┐
+** ┴ ┴└─┘┴└─└─┘┴└─┴ ┴  └  ┴└─┴ ┴┴ ┴└─┘└┴┘└─┘┴└─┴ ┴
+** A Powerful General Purpose Framework
+** More information in: https://aurora-fw.github.io/
+**
+** Copyright (C) 2017 Aurora Framework, All rights reserved.
+**
+** This file is part of the Aurora Framework. This framework is free
+** software; you can redistribute it and/or modify it under the terms of
+** the GNU Lesser General Public License version 3 as published by the
+** Free Software Foundation and appearing in the file LICENSE included in
+** the packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
+****************************************************************************/
+
+#ifndef AURORAFW_CORELIB_TYPETRAITS_H
+#define AURORAFW_CORELIB_TYPETRAITS_H
+
+#include <AuroraFW/Global.h>
+#if(AFW_TARGET_PRAGMA_ONCE_SUPPORT)
+	#pragma once
+#endif
+
+#include <AuroraFW/Internal/Config.h>
+
+namespace AuroraFW {
+	template<typename T>
+	struct AFW_API ActualType {
+		typedef T type;
+	};
+
+	template<typename T>
+	struct AFW_API ActualType<T*> {
+		typedef typename ActualType<T>::type type;
+	};
+}
+
+#endif // AURORAFW_CORELIB_TYPETRAITS_H
